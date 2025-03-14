fbc3a1e6129e8bd7f2616a6772542f5062cf46a5
Agora <167824528+AgoraNZ@users.noreply.github.com>
2025-03-15 10:51:14 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agora-app-cache-v9';
+const CACHE_NAME = 'agora-app-cache-v10';
 const urlsToCache = [
   '/', // Adjust if your entry point is not at the root.
   '/index.html',
